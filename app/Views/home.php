933d310fe11b68590c937a902353ddc05f521667
vojasd <87223187+vojasd@users.noreply.github.com>
2021-08-17 22:51:24 +0200

    vojina treca izmena

--- app/Views/home.php
+++ app/Views/home.php
@@ -7,7 +7,7 @@
         <header class="masthead">
             <div class="container">
                 <div class="masthead-subheading">Welcome!</div>
-                <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
+                <div class="masthead-heading text-uppercase">Zdravo</div>
                 <a class="btn btn-primary btn-xl text-uppercase" href="#services">Info</a>
             </div>
         </header>
