ae2d192f36f56d11f6562b7d9ad84779b0e7fd24
vojislav stojnic <vojislav.stojnic@gmail.com>
2021-08-17 23:24:25 +0200

    Voja7ma

--- app/Views/home.php
+++ app/Views/home.php
@@ -7,7 +7,7 @@
         <header class="masthead">
             <div class="container">
                 <div class="masthead-subheading">Vozdra raja!</div>
-                <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
+                <div class="masthead-heading text-uppercase">Kako je macka</div>
                 <a class="btn btn-primary btn-xl text-uppercase" href="#services">Dobro vece</a>
             </div>
         </header>
