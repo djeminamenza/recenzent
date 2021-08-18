24d621ef0c1b188d7f3f5fbd3d78a0ce4ec7710b
vojislav stojnic <vojislav.stojnic@gmail.com>
2021-08-18 21:57:07 +0200

    Voja11

--- app/Views/menu.php
+++ app/Views/menu.php
@@ -1,5 +1,6 @@
-        <li class="nav-item"><a class="nav-link" href="#services">Services</a></li> 
-        <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
+        <li class="nav-item"><a class="nav-link" href="#services">Servisi</a></li> 
+        <li class="nav-item"><a class="nav-link" href="#portfolio">Naučni rezultati</a></li>
         <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
         <li class="nav-item"><a class="nav-link" href="#team">Team</a></li>
-        <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+        <li class="nav-item"><a class="nav-link" href="#contact">Prijava</a></li>
+        <li class="nav-item"><a class="nav-link" href="">Registracija</a></li>
