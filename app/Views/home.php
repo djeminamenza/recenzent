197861c03eb210d314cd275cafab358ba3d83b5f
Jelena_Stanic <jelenastn30@gmail.com>
2021-08-18 21:22:56 +0200

    11

--- app/Views/home.php
+++ app/Views/home.php
@@ -8,7 +8,7 @@
             <div class="container">
                 <div class="masthead-subheading">Dobrodošli na naš i Vaš portal!</div>
                 <div class="masthead-heading text-uppercase">Recenzije naučnih rezultata</div>
-                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Naš cilj je progres u svim oblastima.</a>
+                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Naš cilj je progres u svim oblastima...</a>
             </div>
         </header>
         <!-- Services-->
