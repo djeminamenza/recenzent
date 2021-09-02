baf1475e65b2705c0c0f41b2bba2c7c928e51247
Jelena_Stanic <jelenastn30@gmail.com>
2021-09-02 16:10:48 +0200

    korisnici/index

--- app/Controllers/Korisnici.php
+++ app/Controllers/Korisnici.php
@@ -2,11 +2,15 @@
 
 namespace App\Controllers;
 
+use App\Models\Status;
+
 class Korisnici extends BaseController
 {
 	public function index()
 	{
-		return view('korisnici/index');
+		$statusi= New Status();
+		$data['status_prijave']=$statusi->findAll();
+		return view('korisnici/index', $data);
 	}
 
     public function prijava()
--- app/Views/korisnici/index.php
+++ app/Views/korisnici/index.php
@@ -8,14 +8,22 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-
-        <h1 style="text-align: center;">Vasa prijava je:</h1>
-
-
     <div class="container">
-      <div class="col">
-        <div class="form-row">
-          <button type="submit" class="btn btn-danger">OK</button>
+    <div class="row">
+		<div class="col-sm-6 offset-sm-3">
+    <div class="card">
+		<h2 class="card-header">Status Vase prijave je:</h2>
+				<div class="card-body">
+      <select name= "status" class="form-control">
+      <?php 
+        foreach($status_prijave as $s){
+          echo'<option value='. $s->id. 'selected>'. $s->opis.'</option>';
+        }
+      ?>
+
+      </select>
+        </div>
+    </div>
         </div>
       </div>
   </div>
